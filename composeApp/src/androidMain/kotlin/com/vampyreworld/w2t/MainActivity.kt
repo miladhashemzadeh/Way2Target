bf9f763a0c012532afe86c8d95f2732a3e699086
M.Hashemzadeh <anik.rpx.06@gmail.com>
2026-05-11 09:39:29 -0700

    migrate from viewModel + nav to decompose + mvi

--- composeApp/src/androidMain/kotlin/com/vampyreworld/w2t/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/vampyreworld/w2t/MainActivity.kt
@@ -25,5 +25,5 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun AppAndroidPreview() {
-    App()
+    //App()
 }
--- core/src/commonMain/kotlin/com/vampyreworld/w2t/core/arch/MviUtils.kt
+++ core/src/commonMain/kotlin/com/vampyreworld/w2t/core/arch/MviUtils.kt
@@ -1,18 +1,17 @@
 package com.vampyreworld.w2t.core.arch
 
-import com.arkivanov.decompose.value.MutableValue
+import com.arkivanov.decompose.Cancellation
 import com.arkivanov.decompose.value.Value
 import com.arkivanov.mvikotlin.core.store.Store
-import com.arkivanov.mvikotlin.rx.Disposable
-import com.arkivanov.mvikotlin.rx.observer
+import com.arkivanov.mvikotlin.core.rx.observer
 
 fun <T : Any> Store<*, T, *>.asValue(): Value<T> =
     object : Value<T>() {
         override val value: T get() = state
 
-        override fun subscribe(observer: (T) -> Unit): com.arkivanov.decompose.value.Disposable {
+        override fun subscribe(observer: (T) -> Unit): Cancellation {
             val disposable = this@asValue.states(observer(onNext = observer))
-            return com.arkivanov.decompose.value.Disposable {
+            return Cancellation {
                 disposable.dispose()
             }
         }
