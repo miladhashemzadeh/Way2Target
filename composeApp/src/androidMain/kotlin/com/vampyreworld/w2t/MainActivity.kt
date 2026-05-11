96cdb0e74a46f77204211f52763b7b5f14c020e6
M.Hashemzadeh <anik.rpx.06@gmail.com>
2026-05-11 08:40:04 -0700

    refactor: update App and MainActivity to use Decompose RootComponent

--- composeApp/src/androidMain/kotlin/com/vampyreworld/w2t/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/vampyreworld/w2t/MainActivity.kt
@@ -6,14 +6,18 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.arkivanov.decompose.defaultComponentContext
+import com.vampyreworld.w2t.root.DefaultRootComponent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
+        val root = DefaultRootComponent(defaultComponentContext())
+
         setContent {
-            App()
+            App(root)
         }
     }
 }
--- composeApp/src/commonMain/kotlin/com/vampyreworld/w2t/App.kt
+++ composeApp/src/commonMain/kotlin/com/vampyreworld/w2t/App.kt
@@ -1,48 +1,26 @@
 package com.vampyreworld.w2t
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.Composable
+import com.arkivanov.decompose.extensions.compose.stack.Children
+import com.arkivanov.decompose.extensions.compose.stack.animation.fade
+import com.arkivanov.decompose.extensions.compose.stack.animation.stackAnimation
+import com.vampyreworld.w2t.root.RootComponent
 import com.vampyreworld.w2t.sharedui.theme.W2TTheme
-import org.jetbrains.compose.resources.painterResource
-
-import way2target.composeapp.generated.resources.Res
-import way2target.composeapp.generated.resources.compose_multiplatform
 
 @Composable
-@Preview
-fun App() {
+fun App(root: RootComponent) {
     W2TTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.background)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
+        Children(
+            stack = root.childStack,
+            animation = stackAnimation(fade()),
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+            when (val child = it.instance) {
+                is RootComponent.Child.Splash -> {
+                    Text("Splash Screen")
+                }
+                is RootComponent.Child.Home -> {
+                    Text("Home Screen")
                 }
             }
         }
